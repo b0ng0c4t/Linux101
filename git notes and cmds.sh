3a4b5b01b36161302e6824cdd925fb0520019576
Kurb4n <alex180588@protonmail.com>
2020-03-19 08:49:52 +0100

    git notes i use

---
+++ git notes and cmds.sh
@@ -0,0 +1,20 @@
+GIT CYCLE > UNMODIFIED (change) > MODIFIED (staging) > STAGED (commit)
+
+
+git config user.name "NAME" #set the commit to this user before do the change
+git config user.email "EMAIL"
+
+git config --global user.name "NAME" #set this user to global user for the repository
+git config --global user.email "EMAIL"
+
+git config --list #see the current settings parameters of the system
+
+git add *.* #track all the files
+git commit -m "MESSAGE COMMIT" #send it to the git folder online (stage the files)
+
+git status
+git log
+git pull #error rejection
+
+git fetch origin master
+git merge origin master

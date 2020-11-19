8b760838668c55931ed01f9ea092ca69c4eb60ab
kurb4n <alex180588@protonmail.com>
2020-11-19 08:57:38 +0100

    cleaning, organizing, updating

--- linux sysadmin 101.sh
+++ linux sysadmin 101.sh
@@ -3,7 +3,16 @@
 
 #-FILE SYSTEM HIERARCHY-#
 /etc #Contains configuration files for programs and packages
-    /etc/passwd #contains passwords and other information concerning users who are registered to use the system. For obvious security reasons, this is writable only by root and readble by others. It can be modified by root directly, but it is preferable to use a configuration utility such as passwd to make the changes. A corrupt /etc/passwd file can easily render a Linux box unusable.
+    /etc/passwd #contains passwords and other information concerning users who are registered to use the system. For obvious security reasons, this is writable only by root and readble by others. It can be modified by root directly, but it is preferable to use a configuration utility such as passwd to make the changes. A corrupt /etc/passwd file can easily render a Linux box unusable.  
+        
+        1-Username: It is used when user logs in. It should be between 1 and 32 characters in length.
+        2-Password: An x character indicates that encrypted password is stored in /etc/shadow file. Please note that you need to use the passwd command to computes the hash of a password typed at the CLI or to store/update the hash of the password in /etc/shadow file.
+        3-User ID (UID): Each user must be assigned a user ID (UID). UID 0 (zero) is reserved for root and UIDs 1-99 are reserved for other predefined accounts. Further UID 100-999 are reserved by system for administrative and system accounts/groups.
+        4-Group ID (GID): The primary group ID (stored in /etc/group file)
+        5-User ID Info: The comment field. It allow you to add extra information about the users such as user’s full name, phone number etc. This field use by finger command.
+        6-Home directory: The absolute path to the directory the user will be in when they log in. If this directory does not exists then users directory becomes /
+        7-Command/shell: The absolute path of a command or shell (/bin/bash). Typically, this is a shell. Please note that it does not have to be a shell. For example, sysadmin can use the nologin shell, which acts as a replacement shell for the user accounts. If shell set to /sbin/nologin and the user tries to log in to the Linux system directly, the /sbin/nologin shell closes the connection.
+
     /etc/group #similar to /etc/passwd but for groups
     /etc/hosts #contains a list of host names and absolute IP addresses.
     /etc/resolv.conf #contains a list of domain name servers used by the local machine
@@ -40,9 +49,12 @@
 
 #-USE SYSTEM DOCUMENTATION-#
 info #Read information files; provides more information than man
+whoami #print the name of the current user
 which #Show full path of command; useful for scripting
 whatis #Display manual page descriptions
 locate #Locate files on system by name
+uptime #shows since how long your system is running and the number of users are currently logged in and also displays load average for 1,5 and 15 minutes intervals.
+W #It will displays users currently logged in and their process along-with shows load averages. also shows the login name, tty name, remote host, login time, idle time, JCPU, PCPU, command and processes.
 updatedb #Update locate command databases
 man #Documentation, 9 sections
     1 #Executable programs and shell commands
@@ -69,6 +81,15 @@ man #Documentation, 9 sections
     systemctl poweroff
     poweroff
     shutdown -P
+
+#-CRONTAB-#
+30 08 10 06 * /home/maverick/full-backup
+•	Minute – A minute value can be between 0-59
+•	Hour – A hour value can be between 0-23
+•	Day_of_the_month – This value can between 1-31. For the months having less days will ignore remaining part
+•	Month_of_the_year – This can be between 1-12. You can also define this value with first three alphebts of month like jan, feb, mar, apr etc.
+•	Day_of_the_Week – This can be the value between 0-7. Where 0 and 7 for Sunday, 1 for Monday, 2 for Tuestday etc. You can also use first three alphabets of days like, sun, mon, tue, wed, etc.
+•	command/script to execute
 
 #-BASIC LINUX COMMANDS-#
 #---->if you need to see the options remember to add -h or -help
@@ -107,6 +128,10 @@ ln #Create links between files. Links directly to an inode to create a new entry
 ln -s #Soft links that connects one file to another, symbolically
 
 #-CHANGE PERMISSIONS-#
+r/4 - read
+w/2 - write
+x/1 - execute
+
 chmod #set the permissions for a file or directory
     -u #User
     -g #Group
@@ -183,6 +208,11 @@ Clean up
 exit
 
 #-CPU/MEMORY COMMANDS-#
+Ps #displays processes running in the system
+Kill #terminate process
+   pkill -9 -t [pts/tty#] #kill user session
+•	TTY: tty stands for Teletypewriter. This is directly connected to the system as a keyboard/mouse or a serial connection to the device (for instance, the console on your system).
+•	PTS: pts Stands for the pseudo terminal slave. It is a terminal device, which is emulated by another program (for instance, ssh session to your system).
 top
     k #Kill process
     q #Quit
@@ -229,3 +259,127 @@ xfs_admin -L LABELNAME /dev/disk #Mount with file system label (XFS)
 mount LABEL=LABELNAME /mnt/mountlocation defaults 1 1 #Mount with label, non-persistent; edit /etc/fstab for persistence
 mount -a #Mount all file systems in /etc/fstab
 umount -a #Unmount all file systems in /etc/fstab
+
+#-ANALYSIS OF LOGS-#
+•	Grep
+#Using surround search returns a number of lines before or after a match. This provides context for each event by letting you trace the events that led up to or immediately followed the event. The -B flag specifies how many lines to return before the event, and the -A flag specifies the number of lines after.
+#For example, let’s search for attempted logins with an invalid username and show the surrounding results. We see that users who fail to log in also fail the reverse mapping check. This means the client doesn’t have a valid reverse DNS record, which is common with public Internet connections. This doesn’t mean your SSH server is vulnerable, but it could mean attackers are actively trying to gain access to it.
+#$ grep -B 3 -A 2 'Invalid user' /var/log/auth.log
+#Apr 28 17:06:20 ip-172-31-11-241 sshd[12545]: reverse mapping checking getaddrinfo for 216-19-2-8.commspeed.net [216.19.2.8] failed - POSSIBLE BREAK-IN ATTEMPT!
+#Apr 28 17:06:20 ip-172-31-11-241 sshd[12545]: Received disconnect from 216.19.2.8: 11: Bye Bye [preauth]
+#pr 28 17:06:20 ip-172-31-11-241 sshd[12547]: Invalid user admin from 216.19.2.8
+#pr 28 17:06:20 ip-172-31-11-241 sshd[12547]: input_userauth_request: invalid user admin [preauth]
+#pr 28 17:06:20 ip-172-31-11-241 sshd[12547]: Received disconnect from 216.19.2.8: 11: Bye Bye [preauth]
+•	Tail
+#Tail is another command line tool that can display the latest changes from a file in real time. This is useful for monitoring ongoing processes, such as restarting a service or testing a code change. You can also use tail to print the last few lines of a file, or pair it with grep to filter the output from a log file.
+#$ tail -f /var/log/auth.log | grep 'Invalid user'
+#Apr 30 19:49:48 ip-172-31-11-241 sshd[6512]: Invalid user ubnt from 219.140.64.136
+#pr 30 19:49:49 ip-172-31-11-241 sshd[6514]: Invalid user admin from 219.140.64.136
+    -tail -n 15 #shows the last 15 lines
+•	AWK
+#awk works on programs that contain rules comprised of patterns and actions. The action is executed on the text that matches the pattern. Patterns are enclosed in curly braces ({}). Together, a pattern and an action form a rule. The entire awk program is enclosed in single quotes (').
+#awk considers a field to be a string of characters surrounded by whitespace, the start of a line, or the end of a line. Fields are identified by a dollar sign ($) and a number. So, $1 represents the first field, which we’ll use with the print action to print the first field.
+who | awk '{print $1,$4}'
+•	$0: Represents the entire line of text.
+•	$1: Represents the first field.
+•	$2: Represents the second field.
+•	$7: Represents the seventh field.
+•	$45: Represents the 45th field.
+•	$NF: Stands for “number of fields,” and represents the last field.
+#Examples:
+1) Amit     Physics   80
+2) Rahul    Maths     90
+3) Shyam    Biology   87
+4) Kedar    English   85
+5) Hari     History   89
+
+awk '{print $3 "\t" $4}' marks.txt
+Output
+Physics   80
+Maths     90
+Biology   87
+
+
+#-NETWORKING COMMANDS-#
+/etc/sysconfig/network-scripts #is the location where all config files are located
+ifconfig #this command is use to initialize an interface, assign IP Address to interface and enable or disable interface on demand. 
+iwconfig #same that ifconfig but with wireless
+ip addr #The ip command allows Linux users to quickly leverage the IP utility for manipulating the routing, devices, policy routing, and tunnels.
+nslookup #obtain domain name or IP address mapping by querying the Domain Name System
+         #"name server lookup", finds information about a named domain. We can also perform the above operation in reverse by providing the IP address rather than the domain name.
+         #-type=ns/a/aaaa/mx/cname
+wget #tool that allow to downloading contents from web servers.
+dig #for query the DNS for network tshooting
+whois #tool for obtaining both domain and IP related information about a network.
+ping
+traceroute
+netstat
+    -a #list all ports
+    -at #list all TCP ports
+    -au #list all UDP ports
+    -l #list only listening ports
+    -lt #list only TCP listening ports
+    -lu #list only UDP listening ports
+    -s #show statistics for all ports
+
+#---> NETWORK MANAGER
+nmcli con show #display lisy of connections
+    --active #display only active connections
+nmcli dev status #display the device status
+nmcli con add con-name “nameofthecon” type Ethernet ifname eth0 #add a connection
+    -- ifname is the name of the Ethernet device; can find them by doing ls /sys/class/net
+nmcli con reload #after modifying a network connection you should reload the config.
+nmcli dev disconnect "device" #turn off the network device
+nmcli dev connect "device" #turn on the network device
+systemctl start NetworkManager
+systemctl enable NetworkManager
+systemctl restart network
+
+
+#-LOGICAL VOLUME MANAGER-#
+yum install lvm2 #install LVM if not installed
+pvcreate /dev/disk /dev/disk [disk-amount] #Create LVM physical volumes
+pvdisplay #display all available physical volumes
+vgcreate VGNAME /dev/disk /dev/disk #create a volume group. VGNAME is the name you assign to the volume group
+vgdisplay #display all the available volume groups
+lvcreate -n NameOfLVMVolume -L 20G VolumeGroupToUse #Create a logical volume:
+    -L #size of the volume in bytes (use 1M, 1G, 20G, etc.)
+    -l #size in physical extents; generally a physical extent is 4MiB in size
+lvdisplay #Display view logical volumes:
+
+#--->EXTENDING VOLUME GROUPS AND LOGICAL VOLUMES
+1- Extend a volume group with a new disk:
+    - pvcreate /dev/newdisk
+    - vgextend vg-name /dev/newdisk
+    - vgdisplay
+2.1- Extend the LVM to be exactly XGBs in size:
+    - lvextend -L XG /dev/vg-name/lvmname
+2.2- Extend the LVM and add an additional 5G:
+    - lvextend -L +5G /dev/vg-name/lvmname
+
+#---> REMOVING LOGICAL AND PHYSICAL VOLUMES & VOLUME GROUPS
+lvremove /dev/vg-name/volume #remove a logical volume
+vggremove vg-name #Remove a volume group
+
+#-IPTABLES-#
+systemctl start iptables
+systemctl stop iptables
+systemctl restart iptables
+iptables -L -n -v #check all IPTABLES firewall rules
+iptables -A INPUT -s xxx.xxx.xxx.xxx -j DROP #block specific IP address in Iptables
+iptables -D INPUT -s xxx.xxx.xxx.xxx -j DROP #unblock specific IP address in Iptables
+iptables -A OUTPUT -p tcp --dport xxx -j DROP #block specific port
+iptables -A INPUT -p tcp --dport xxx -j ACCEPT #allow specific port
+#allow multiple ports on Iptables using Multiport
+iptables -A INPUT  -p tcp -m multiport --dports 22,80,443 -j ACCEPT
+iptables -A OUTPUT -p tcp -m multiport --sports 22,80,443 -j ACCEPT
+#block a specific range of addresses based on a hostname
+1- get the public IP address with the host command
+host facebook.com 
+-->facebook.com has address 66.220.156.68
+2- get the CIDR bassed in the public address
+whois 66.220.156.68 | grep CIDR
+-->CIDR: 66.220.144.0/20
+3- create the rules
+iptables -A OUTPUT -p tcp -d 66.220.144.0/20 -j DROP
+

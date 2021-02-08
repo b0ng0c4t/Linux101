8d3a61f69ce81b0720796017939a7cb918673e3e
kurb4n <alex180588@protonmail.com>
2021-02-08 18:33:24 +0100

    adding TMUX cheatsheet

--- Cheatsheets programs i use.sh
+++ Cheatsheets programs i use.sh
@@ -1,4 +1,13 @@
+--VS CODE
 #find settings.json
 Windows %APPDATA%\Code\User\settings.json
 macOS $HOME/Library/Application Support/Code/User/settings.json
 Linux $HOME/.config/Code/User/settings.json
+
+--TMUX
+ctrl+b = bind key
+bind+c = new tab
+bind+, = rename tab
+bind+x = kill tab
+bind+% = split panel vertically
+bind+" = split panel horizontaly

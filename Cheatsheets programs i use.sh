8d7272eb930976cf1fe94115e3271ab077859579
kurb4n <alex180588@protonmail.com>
2021-04-15 17:19:50 +0200

    update docker file, added VSCode shortcut

--- Cheatsheets programs i use.sh
+++ Cheatsheets programs i use.sh
@@ -3,6 +3,9 @@
 Windows %APPDATA%\Code\User\settings.json
 macOS $HOME/Library/Application Support/Code/User/settings.json
 Linux $HOME/.config/Code/User/settings.json
+#tab several lines in VSCode
+control + shift and select the lines you want to indent
+control + ] to tab them
 
 --TMUX
 ctrl+b = bind key

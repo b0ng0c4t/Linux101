dabe63838693ac35788aa89dd9eb0428dae48306
kurb4n <alex180588@protonmail.com>
2021-04-15 17:21:22 +0200

    update docker file, added VSCode shortcut

--- git notes and cmds.sh
+++ git notes and cmds.sh
@@ -1,5 +1,5 @@
 GIT CYCLE > UNMODIFIED (change) > MODIFIED (staging) > STAGED (commit)
-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
+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
 
 git config user.name "NAME" #set the commit to this user before do the change
 git config user.email "EMAIL"
